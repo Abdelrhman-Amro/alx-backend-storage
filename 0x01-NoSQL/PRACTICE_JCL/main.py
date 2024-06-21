7362ad1b0cbb12b325d4349ec746f27e87f6708a
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-06-21 23:27:29 +0300

    redis

--- 0x01-NoSQL/PRACTICE_JCL/main.py
+++ 0x01-NoSQL/PRACTICE_JCL/main.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python3
+
 import os
 from pymongo import MongoClient
 from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+connection_string = os.getenv('MONGO_CONNECTION_STRING')
+client = MongoClient(connection_string)
+
+dbs = client.list_database_names()
+print(dbs)
+learn_Mongo_DB = client.learn_Mongo_DB
+collections = learn_Mongo_DB.list_collection_names()
+print(collections)
